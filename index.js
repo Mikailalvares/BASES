8cbc907b5e9ead8727b7825ed51a3de90fefc503
Mikailalvares <mikailismail260@gmail.com>
2023-09-30 11:19:18 -0900

    complete

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const commentList = document.getElementById("comment-list");
-    const commentForm = document.getElementById("comment-form");
+document.addEventListener("DOMContentLoaded", () => {
+    const apiUrl = "http://your-api-url.com"; // Replace with your API URL
 
-    commentForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const nameInput = document.getElementById("name");
-        const commentInput = document.getElementById("comment");
-
-        const name = nameInput.value;
-        const comment = commentInput.value;
-
-        if (name && comment) {
-            addComment(name, comment);
-            nameInput.value = "";
-            commentInput.value = "";
+    // Function to fetch the list of teachers
+    async function fetchTeacherList() {
+        try {
+            const response = await fetch(`${apiUrl}/teachers`);
+            if (!response.ok) {
+                throw new Error("Error fetching teacher list");
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching teacher list:", error);
+            return [];
         }
-    });
-
-    function addComment(name, comment) {
-        const commentItem = document.createElement("div");
-        commentItem.classList.add("comment");
-        commentItem.innerHTML = `
-            <strong>${name}:</strong>
-            <p>${comment}</p>
-        `;
-        commentList.appendChild(commentItem);
     }
+
+    // Function to display the list of teachers
+    function displayTeacherList(teachers) {
+        const teacherList = document.getElementById("teacher-list");
+        teacherList.innerHTML = "";
+        teachers.forEach((teacher) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = teacher.name;
+            listItem.dataset.id = teacher.id;
+            listItem.addEventListener("click", () => {
+                displayTeacherDetails(teacher);
+            });
+            teacherList.appendChild(listItem);
+        });
+    }
+
+    // Function to display teacher details
+    function displayTeacherDetails(teacher) {
+        const teacherName = document.querySelector("#teacher-details h2");
+        const teacherImage = document.getElementById("teacher-image");
+        const teacherDescription = document.getElementById("teacher-description");
+
+        teacherName.textContent = teacher.name;
+        teacherImage.src = teacher.image;
+        teacherDescription.textContent = teacher.description;
+    }
+
+    // Initial setup: Fetch and display the list of teachers
+    fetchTeacherList().then((teachers) => {
+        displayTeacherList(teachers);
+    });
 });
